973c792055a90cc42aff2cd7601aa098704f0217
kimsey3633 <kimsey3633@naver.com>
2024-12-01 01:30:02 +0900

    test

--- CLiveDlg.cpp
+++ CLiveDlg.cpp
@@ -85,6 +85,7 @@ BOOL CLiveDlg::OnInitDialog()
 void CLiveDlg::OnTimer(UINT_PTR nIDEvent)
 {
 	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	//testing
 	if (nIDEvent == 0) {
 		showRandomChoice(false);
 	}
