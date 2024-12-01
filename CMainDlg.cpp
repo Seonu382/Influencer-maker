8996ec0a5280ff67011a8fec4234c81e671ec390
kimsey3633 <kimsey3633@naver.com>
2024-12-01 15:02:29 +0900

    Added Res and Changed main

--- CMainDlg.cpp
+++ CMainDlg.cpp
@@ -145,7 +145,9 @@ BOOL CMainDlg::OnInitDialog()
 	CStatic* p_lamp_image = (CStatic*)GetDlgItem(IDC_MAIN);
 	CBitmap lamp_image;
 
-	if (Player::instance.GetFollower() >= 100000)
+	if (Player::instance.GetFollower() >= 500000)
+		lamp_image.LoadBitmap(IDB_MAIN_HYPER);
+	else if (Player::instance.GetFollower() >= 100000)
 		lamp_image.LoadBitmap(IDB_MAIN_HIGH);
 	else
 		lamp_image.LoadBitmap(IDB_MAIN_LOW);
--- resource.h
+++ resource.h
@@ -43,6 +43,7 @@
 #define IDB_BITMAP2                     177
 #define IDB_START                       177
 #define IDB_ICON_FOLLOWER               179
+#define IDB_MAIN_HYPER                  181
 #define IDC_START_BUTTON                1000
 #define IDC_NAME                        1002
 #define IDC_NAME_EDIT                   1003
@@ -96,7 +97,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        181
+#define _APS_NEXT_RESOURCE_VALUE        182
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1045
 #define _APS_NEXT_SYMED_VALUE           101
